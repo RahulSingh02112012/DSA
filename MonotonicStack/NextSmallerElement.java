964d4089c9ec3ecd76e5d2aebba0887ac99ac285
Rahul Singh <rahulsingh02112012@gmail.com>
2024-04-08 10:49:43 +0530

    Added New Codes

--- MonotonicStack/NextSmallerElement.java
+++ MonotonicStack/NextSmallerElement.java
@@ -1,29 +1,27 @@
 package Tree.MonotonicStack;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class NextGreaterElement {
-    //arr = [13, 8, 1, 5, 2, 5, 9, 7, 6, 12]
+public class NextSmallerElement {
+
     public static void main(String[] args) {
         int[] input = new int[] {13, 8, 1, 5, 2, 5, 9, 7, 6, 12};
-        int[] res = nextGreaterElementIndexes(input);
+        int[] res = nextSmallerElement(input);
 
         System.out.println(Arrays.stream(input).boxed().collect(Collectors.toList()));
         System.out.println(Arrays.stream(res).boxed().collect(Collectors.toList()));
 
     }
 
-    private static int[] nextGreaterElementIndexes(int[] nums) {
+    private static int[] nextSmallerElement(int[] nums) {
         int[] res = new int[nums.length];
         Stack<Integer> stk = new Stack<>();
         Arrays.fill(res,-1);
 
         for(int i =0;i< nums.length;i++) {
-            while(!stk.isEmpty() && nums[stk.peek()] < nums[i]) {
+            while(!stk.isEmpty() && nums[stk.peek()] > nums[i]) {
                 int top = stk.pop();
                 res[top] = nums[i];
             }
