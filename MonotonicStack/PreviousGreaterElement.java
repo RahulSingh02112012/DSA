d2b477164a71834d5ddbfc2e161473e65d07cfdd
Rahul Singh <rahulsingh02112012@gmail.com>
2024-04-08 10:23:51 +0530

    Added New Codes

--- MonotonicStack/PreviousGreaterElement.java
+++ MonotonicStack/PreviousGreaterElement.java
@@ -8,14 +8,14 @@ public class PreviousGreaterElement {
 
     public static void main(String[] args) {
         int[] input = new int[] {13, 8, 1, 5, 2, 5, 9, 7, 6, 12};
-        int[] res = nextGreaterElementIndexes(input);
+        int[] res = previousGreaterElement(input);
 
         System.out.println(Arrays.stream(input).boxed().collect(Collectors.toList()));
         System.out.println(Arrays.stream(res).boxed().collect(Collectors.toList()));
 
     }
 
-    private static int[] nextGreaterElementIndexes(int[] nums) {
+    private static int[] previousGreaterElement(int[] nums) {
         int[] res = new int[nums.length];
         Stack<Integer> stk = new Stack<>();
         Arrays.fill(res,-1);
